c82ceeb3576396dd089948e06f642b47f8872617
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2022-02-20 06:03:01 +0100

    initial

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+let
+  sources = import ./nix/sources.nix {};
+
+  haskellNix = import sources.haskellNix {};
+
+  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
+
+  tasklite-src = pkgs.fetchFromGitHub {
+    owner = "ad-si";
+    repo = "tasklite";
+    rev = "master";
+    sha256 = "kxX5U2lMqRJX9TvYOIdngGEd5zCNqvNGF2IRSLWUAqU=";
+  };
+  project = pkgs.haskell-nix.project {
+    src = pkgs.haskell-nix.haskellLib.cleanGit {
+      name = "tasklite";
+      src = tasklite-src;
+    };
+
+    modules = [{
+      packages.tasklite-core.patches = [ ./nix/dont-get-version-from-git.patch ];
+    }];
+  };
+in
+  project.tasklite-core.components.exes.tasklite
+
